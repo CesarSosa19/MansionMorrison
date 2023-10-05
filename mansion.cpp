b596c13f0bca18ef95ee39e3d9e9cf5ab9a98108
Cesar Sosa <126929576+CesarSosa19@users.noreply.github.com>
2023-10-05 17:55:55 -0600

    actualizado

--- mansion.cpp
+++ mansion.cpp
@@ -4,10 +4,13 @@
 
 int main(int argc, char const *argv[])
 {
+  char nombreJugador[21];
+
+
     printf(" \
-                                           ,,                                                    ,,                                \n \
-`7MMF'  `7MMF'            mm             `7MM      `7MMM.     ,MMF'                              db                                \n \
-  MM      MM              MM               MM        MMMb    dPMM                                                                  \n \
+                                           ,,                                                    ,,\n \
+`7MMF'  `7MMF'            mm             `7MM      `7MMM.     ,MMF'                              db\n \
+  MM      MM              MM               MM        MMMb    dPMM\n \
   MM      MM   ,pW'Wq.  mmMMmm   .gP Ya    MM        M YM   ,M MM   ,pW'Wq.  '7Mb od8  7Mb od8  7MM   pP'Ybd   pW Wq    7MMpMMMb   \n \
   MMmmmmmmMM  6W'   'Wb   MM    ,M    Yb   MM        M  Mb  M' MM  6W     Wb   MM'  '   MM'  '   MM  8I      6W     Wb   MM    MM  \n \
   MM      MM  8M     M8   MM    8Moooooo   MM        M  YM.P'  MM  8M     M8   MM       MM       MM  `YMMMa. 8M     M8   MM    MM  \n \
@@ -25,56 +28,10 @@ int main(int argc, char const *argv[])
     getch();
     system("cls");
 
-    printf(" \
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n \
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n \
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMX0OOOO0OO00OOOOOOOOOOOOOOOOOOOOOOOOOO00OOO0NMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n \
-MMMMMMMMMMMMMMMMMMMMMMMMWWX0o'........................................'o0XWMMMWMMMMMMMMMMMMMMMMMMMMM\n \
-MMMMMMMMMMMMMMMMMMWMMWMMX0l',:::::clollooooooooooooooooooooooooolooolol;'o0XMMMMMMMMMMMMMMMMMMMMMMMM\n \
-MMMMMMMMMMMMMMMMMWWMWWNKo.,c,......:loodddddddddddddddddddddddddddoddddol;'lKNWWMMMMMMMMMMMMMMMMMMMM\n \
-MMMMMMMMMMMMMMMMMMMWXKo';:,..,;;;;'.':loodddodddddddddddddddddddoodddddddll:'l0XWMMMMMMMMMMMMMMMMMMM\n \
-MMMMMMMMMMMMMMMMWWNKl';:,.':clllllll;'':oodddodddddddddddddddddddddddddddodol;'lKNWWWWMMMMMMMMMMMMMM\n \
-MMMMMMMMMMMMMMMWNKl',:,.';ccccccllolc:;'.:ooddddddddddddddddddddddddddddddoddol;'lKNWWWMMMMMMMMMMMMM\n \
-MMMMMMMMMMMMMWNKl';:'..,.............;:;,..:ooodddddddddddddddddddddddddoddoddollc;lKWWMMMMMMMMMMMMM\n \
-MMMMMMMMMMMWWKl,::'.,::c'  .......  .;lccc;'.;oooodddoddodddddddddddddddddodddddxdl:,lKWWMMMMMMMMMMM\n \
-MMMMMMMMMWW0c,:;'.,:clll' .,;,''''. .:olloc. ..:ooddodddddddddddddddddddddddddxddoooc:,c0WWMMMWWMMMM\n \
-MMMMMMWWW0c,:;'.',;:cc::,.......... .;c::c;. .'..;looodddddddddddddddddddddxxdddddoodoc:,:0WWMMWWMMM\n \
-MMMMMMW0c;:;'.,:clcclccc'           .;ccclc. 'l:;'..;ododdddddddddddddddddddddddddoddodoc:;cOMMWMMMM\n \
-MMMMW0c;:,'.,:coollllool:,,,,;,,,,;,;lolloc. ,ollc;;'.;oddddddodddddddddddooddddddoddooodo:;;c0MWMMM\n \
-MMWOc;;,'. .,cc:ccc:cccccccccccccccccccccc:. 'cc::cc' ..;lodoodddodddoooodddodddoddddoodddooc;:cOWWM\n \
-WOc;;,'''. .;lccccllcccccccccccccccccccccc:. 'cccccc'  ...;ododddddddddddddddddddddddddddddddoc;::OM\n \
-Wo .'. .;, .:lll,....''.........,collollloc. ,l;.''.. .;'  .''''''''''''''''''''''''''''''''''''  oW\n \
-W0ooo; .;,..;cc;.   ...   ...   .;ccccc::c:. ':.      .;'   .  ..             .. .coooo:. ..  ,ooo0M\n \
-MMMMMx..:;..;cc:.  .',.....,..  .:cccccccc:. ':.  ..  .;' .'. .;:. ........  .c' ;XMMMM0' ,;..xMMMMM\n \
-MMMMMk..:, .:ooc. ..','...',,'. .colllllllc. ,l'  ... .:;..'. .:c. ......... .l, ;XMMMM0' ,;..xMMMMM\n \
-MMMMMx..;, .;cc:. ..','..'',''. .:cccccccc:. 'c. .... 'c,  .. .;:. ......... .c, ;XMMMM0' ,;..xMMMMM\n \
-MMMMMx..;, .;cc:.   ...   ...   .;cccccccc:. ':. ......;'     .;;.           .c' ;XMMMM0' ,;..xMMMMM\n \
-MMMMMx..;, .:olc,...............,collollllc. ,l;';;,. .;'  ...'cc,...........;l, ;XWWMMO' ,;..xMMMMM\n \
-MMMMMx..;, .;lccccccclccccccccccccccccccccc. ,oooooo, .;' .ldooooooodooooooooooc'c0KKKXx. ,;..xMMMMM\n \
-MMMMMx..;,  ;c:cc:c::cc:ccccccccc:cc:::c:c:. ;oooooo, .;' .ldoddddodddddoooddddl:cdxOO0d. ,;..xMMMMM\n \
-MMMMWx..;, .:llolllllllllllllllllllllllllol. ,olllll, .;' .clloddolllodolloodol, .lONWMO. ,;..xMMMMM\n \
-MMMWWd..;, .;llcccccccccccccccclllccllllclc. ,lclllc' .;' .:lclddlcclodocllodol' .oONWMO' ,;..dMMMMM\n \
-WWMXOc .'.  .,,'''''''''''''''','',,,'''','. .,',,,'. .'. .',';::,'',::;,,,;c:,. .;ok0Oo. .'  c0XMMM\n \
-WX0l'.............................''................................................'''........'o0XM\n \
-0l',cooloollooloooooooooooooooooodooooolooloolooloooloooooooolooooooolooooooooooodddoooooooooool;.l0\n \
-..:ccllcccclccclccccccccllclooolcccccccclccllcclcclccllccccccclcclllllllcccccllooolclcclllccllccl:'.\n \
-..'''......................''............... ........ ... ...................''....''''........'''..\n \
-KKKKKo..,'  ,ccccccccccclllccccccccccccccc:. 'cccccc' .,. .:ccccccccccccccllllc' ,OKKKKx. ',. oKKKKK\n \
-MMMWWx..;' .:llllccccccclcccccccccccccclllc. ,lccclc' .;' .:lccllllllllllllllll, ;XMMMMO' ,;..dMMMMM\n \
-MMMMMx..;, .;c::. .....................;:::. ':'....  .;'  ....;:::c::::::::::c' ;XMMMM0' ,;..xMMMMM\n \
-MMMMMx..;, .;ll:.  .  .'..... .... .   ;llc. ,c.  ..  .;'  .. .:lclcccccccccccl' ;XMMMM0' ,;..xMMMMM\n \
-MMMMMx..;, .:ll:.  ...',.......,'....  ;llc. ,l. .... .;.  .. .:loc'.........,l, ;XMMMM0' ,;..xMMMMM\n \
-MMMMMx..;, .;c:;. .''',,''''''',,'''.  ,c:;. ':.  ..  .;'  .. .,:c;. .'..... .:' ;XMMMM0' ,;..xMMMMM\n \
-MMMMMx..;,..:ll:. ....','......,'....  ;ll:. 'c.      .;'   . .;llc. .,,,,,. .c, ;XMWMMO' ,;..xMMMMM\n \
-MMMMMx..:;..:loc.  ....,.......,'.... .:ooc. ,lc;,;,. .;'..;:::lloc. .;,,,,. 'l, ;XWWWWO. ,;..xMMMMM\n \
-MMMMMk..:, .;cc;.      .       ..      ,cc:. ;oooddo, .:;.'lddoddooc:clllllc:col:okO0OOd. ,;..xMMMMM\n \
-MMMMMx..;' .;ccc;,,,,,,,,,,,,,,,,,,,,,;ccc:. ,oooooo;.'c, .loooddol;,ldlc:coddo:';ok0KKx. ,;..xMMMMM\n \
-WWMMMx..;, .:ollooooooolllllllllllllllllloc. ,lloooo:..;' .clllddoc. :dc,,,cdol, .oONWMO' ,;..xMMMMM\n \
-WMMMMx..;, .;ccccccccccccccccccccccccccccc:. 'ccclol' .;' .:cccddl:. :dc,,;cdoc' .oONWW0' ,;..xMMMMM\n \
-MMMMMx. ..  ...............................  ........  ..  ....''..  .,.....,'.. .';ccl;. .. .xWMMMM\n \
-MMMMM0olllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllldKMMMMM\n \
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n \
-kxxxxxxxxxxxxxxxxxxxxxxkxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxkkkxxxxxxkkxxxxxxkkkxxxxxxxxxxxxxxx\n");
-    
+    printf("\nAntes de comenzar necesito saber tu nombre");
+    printf("\nIngresa tu nombre aqui: ");
+    getch();
+    system("cls");
+
     return 0;
 }
